cd423a9cc1d822b0c65b7f3e2ef97f1abd12bf5d
Uglypr1nces <shpatavdiu4@gmail.com>
2024-05-13 17:32:25 +0200

    Update contact information and date in application documents

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from changer.wordtopdf import *
 import datetime
 
 date = datetime.datetime.now()
-
-print(str(date.strftime("%d.%m.%Y")))
+date = str(date.strftime("%d.%m.%Y"))
 
 Firma = ""
 BewerbugnsAdresse = ""
@@ -26,4 +25,12 @@ while True:
         replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", "SDF)Pasdfunpoiva)PHDF(P)S", Firma) #change the company file
         replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", "()SDFZ=ZHVHHHDUC=P/S78z0f", BewerbugnsAdresse) #change address file
         replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", "c6t89fdsa08967tg", BewerbugnsOrt) #change address file
+        replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", "DatummutaD", date)
         word_to_pdf_converter() #convert the word file to pdf and move it
+
+        waiting_animation(3)
+        #change the files back to the original
+        replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", Firma, "SDF)Pasdfunpoiva)PHDF(P)S") 
+        replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", BewerbugnsAdresse, "()SDFZ=ZHVHHHDUC=P/S78z0f") 
+        replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", BewerbugnsOrt, "c6t89fdsa08967tg") 
+        replace_text_in_docx("Bewerbung/Shpat Avdiu.docx", date, "DatummutaD")
